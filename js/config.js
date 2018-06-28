805462020ac530a8ea0d3401b193a9e4e135c202
Daniel Kostro <kostro.d@gmail.com>
2018-06-28 16:55:19 +0200

    change environment configuration variables

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   mail: {
-    host: process.env.MAIL_HOST,
-    user: process.env.MAIL_USER,
-    password: process.env.MAIL_PASSWORD
+    host: process.env.SMTP_HOST,
+    user: process.env.SMTP_USER,
+    password: process.env.SMTP_PASSWORD
   },
   roc: {
     host: process.env.ROC_HOST
